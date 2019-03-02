1fd0cd667d411001062cd0703d6648d98f7680d8
YukiVerrckt <823528848@qq.com>
2019-03-02 15:33:54 +0800

    第二次作业

---
+++ Homework2_1/Homework2_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2_2
+{
+    class Program
+    { 
+
+        static void Main(string[] args)
+        {
+            int[] randArray = new int[10];
+            int sum = 0;
+            float avg;
+            int max=0, min=99;
+            Random rnd = new Random();
+            Console.Write("随机数组为： ");
+            for(int i=0;i<randArray.Length;i++)     //生成随机数组,并计算
+            {
+                randArray[i] = rnd.Next(20);
+                Console.Write(" " + randArray[i]);
+                sum = sum + randArray[i];
+                max = randArray[i] > max ? randArray[i] : max;
+                min = randArray[i] < min ? randArray[i] : min;
+            }
+            avg = sum / randArray.Length;
+            Console.WriteLine("\n平均值：" + avg + ",总和：" + sum + "，最大值：" + max + "，最小值：" + min);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework2_1/Homework2_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("素数：" + 2);
+            for(int i=2;i<=100;i++)
+                for (int j = 2; j < i;j++)
+                {
+                    if (i % j == 0 && i != j)
+                        break;
+                    else if (j == i - 1)
+                        Console.Write(" " + i);
+                }
+            Console.ReadLine();
+        }
+    }
+}
