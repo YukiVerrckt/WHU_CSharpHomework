a53eb89d9d7695c725332c24950db85a17008ca5
YukiVerrckt <823528848@qq.com>
2019-03-02 14:32:13 +0800

    第二次作业第一题

---
+++ Homework2_1/Homework2_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            string s;
+            Console.Write("输入一个自然数：");
+            s = Console.ReadLine();
+            num = int.Parse(s);         //获取输入
+            for(int i=2;i<=num;)
+            {
+                if (num % i == 0)
+                {
+                    Console.Write(" "+i);       //输出因子
+                    num = num / i;
+                }
+                else i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
