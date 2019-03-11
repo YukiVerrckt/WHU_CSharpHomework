bedeb037c26991827b4da74ba98b3545efdd4060
YukiVerrckt <823528848@qq.com>
2019-03-11 16:46:22 +0800

    第三次作业2019.3.11

---
+++ Homework3/ConsoleApplication1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+namespace Homework3_2 //桥接模式
+{
+    class Matrix        //矩阵辅助类
+    {                 //此处代码省略		
+    }
+    
+    interface ImageImp { void doPaint(Matrix m); }//显示像素矩阵m	
+    abstract class Image  //抽象图像类
+        {
+         protected ImageImp imp;
+
+         public void setImageImp(ImageImp imp)
+            {
+                this.imp = imp;
+            }
+         public abstract void parseFile(string fileName);
+        }
+    
+        class WindowsImp:ImageImp
+        {
+            public void doPaint(Matrix m)
+            {	//调用Windows系统的绘制函数绘制像素矩阵	
+                Console.WriteLine(" 在Windows操作系统中显示图像");
+            }
+        }
+        class LinuxImp : ImageImp
+        {
+            public void doPaint(Matrix m)
+            {
+                Console.WriteLine(" 在Linux操作系统中显示图像");
+            }
+        }
+        class UnixImp : ImageImp
+        {
+            public void doPaint(Matrix m)
+            {
+                Console.WriteLine(" 在Unix操作系统中显示图像");
+            }
+        }
+
+        class JPGImage:Image
+        {
+            public override void parseFile(string fileName)
+            {//模拟解析JPG文件并获得一个像素矩阵对象m;	
+                Matrix m = new Matrix();
+                imp.doPaint(m);
+                Console.WriteLine(fileName + ", 格式为JPG");
+            }
+        }
+        class PNGImage : Image
+        {
+            public override void parseFile(string fileName)
+            {
+                Matrix m = new Matrix();
+                imp.doPaint(m);
+                Console.WriteLine(fileName + ", 格式为PNG");
+            }
+        }
+        class GIFImage : Image
+        {
+            public override void parseFile(string fileName)
+            {
+                Matrix m = new Matrix();
+                imp.doPaint(m);
+                Console.WriteLine(fileName + ", 格式为GIF");
+            }
+        }
+        class BMPImage : Image
+        {
+            public override void parseFile(string fileName)
+            {
+                Matrix m = new Matrix();
+                imp.doPaint(m);
+                Console.WriteLine(fileName + ", 格式为BMP");
+            }
+        }
+        
+
+        class Client
+        {
+        static void Main(string[] args)
+        {
+            Image image;
+            ImageImp imp;
+            image = new JPGImage();
+            imp = new WindowsImp();
+            image.setImageImp(imp);
+            image.parseFile("桥接");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework3/Homework3_1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3_1   //工厂模式
+{
+    interface Logger { void writeLog(); }
+
+    interface LoggerFactory { Logger createLogger(); Logger createLogger(object obj); Logger createLogger(string args); }
+
+    class DatabaseLogger : Logger
+    {
+        public void writeLog()
+        { Console.WriteLine("数据库日志记录。"); }
+    }
+
+    class FileLogger : Logger
+    {
+        public void writeLog()
+        { Console.WriteLine("文件日志记录。"); }
+    }
+
+    class DatabaseLoggerFactory:LoggerFactory
+    {
+        public Logger createLogger()
+        {
+            //使用默认方式连接数据库，代码省略		
+            Logger logger = new DatabaseLogger();
+            //初始化数据库日志记录器，代码省略		
+            return logger;
+        }
+        public Logger createLogger(string args)
+        {   //使用参数args作为连接字符串来连接数据库，代码省略		
+            Logger logger = new DatabaseLogger();
+            //初始化数据库日志记录器，代码省略		
+            return logger;
+        }
+        public Logger createLogger(object obj)
+        {   //使用封装在参数obj中的连接字符串来连接数据库，代码省略			
+            Logger logger = new DatabaseLogger();
+            //使用封装在参数obj中的数据来初始化数据库日志记录器，代码省略	
+            return logger;
+        }
+    }
+    class FileLoggerFactory : LoggerFactory
+    {
+        public Logger createLogger()
+        {
+            //使用默认方式连接文件，代码省略		
+            Logger logger = new FileLogger();
+            //初始化文件日志记录器，代码省略		
+            return logger;
+        }
+        public Logger createLogger(string args)
+        {   //使用参数args作为连接字符串来连接文件，代码省略		
+            Logger logger = new FileLogger();
+            //初始化文件日志记录器，代码省略		
+            return logger;
+        }
+        public Logger createLogger(object obj)
+        {   //使用封装在参数obj中的连接字符串来连接文件，代码省略			
+            Logger logger = new FileLogger();
+            //使用封装在参数obj中的数据来初始化文件日志记录器，代码省略	
+            return logger;
+        }
+    }
+
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            LoggerFactory factory;
+            Logger logger;
+            factory = new FileLoggerFactory();
+            logger = factory.createLogger();
+            logger.writeLog();
+            Console.ReadLine();
+        }
+    }
+}
