76a60d295986f8f38a6fd03688ee53193583def7
YukiVerrckt <823528848@qq.com>
2019-05-09 18:58:56 +0800

    第十次作业

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+
+        public string clientName {get;set;}
+        public float price { get; set; }
+        public string detail { get; set; }
+    }
+}
